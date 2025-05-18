b5ef9227fdf51bbb0e4f26107d650acadaa47ac9
bernardoadca18 <bernardoalvesaguiar16@gmail.com>
2025-05-18 14:10:29 -0300

    feat: Backend criado.

---
+++ backend/simgame/src/main/java/com/bsim/backend/simgame/SimgameApplication.java
@@ -0,0 +1,13 @@
+package com.bsim.backend.simgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimgameApplication.class, args);
+	}
+
+}
---
+++ backend/simgame/src/test/java/com/bsim/backend/simgame/SimgameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsim.backend.simgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimgameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
